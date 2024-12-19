4972b5b09065715f664964d1c751e79014292d43
Saif Ur Rehman <bonvages6@gmail.com>
2024-12-19 23:53:04 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+
+// let user ="john \n";
+// let age= "25\n ";
+// let message ='hello \n';
+// user , age , message legal variables
+// 4545 , alert , *jss illegal variables
+let a=5;
+let b=3;
+console.log(a+b);
+console.log("5+3=",a+b)
+console.log("5-3=",a-b)
+console.log("5/3=",a/b)
+console.log("5*3=",a*b)
+
+// a. Declare a variable.
+var myVariable;
+
+// b. Show the value of variable in your browser.
+document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+// c. Initialize the variable with some number.
+myVariable = 5;
+
+// d. Show the value of variable in your browser.
+document.write("Initial value: " + myVariable + "<br>");
+
+// e. Increment the variable.
+myVariable++;
+
+// f. Show the value of variable in your browser.
+document.write("Value after increment is: " + myVariable + "<br>");
+
+// g. Add 7 to the variable.
+myVariable += 7;
+
+// h. Show the value of variable in your browser.
+document.write("Value after addition is: " + myVariable + "<br>");
+
+// i. Decrement the variable.
+myVariable--;
+
+// j. Show the value of variable in your browser.
+document.write("Value after decrement is: " + myVariable + "<br>");
+
+// k. Show the remainder after dividing the variable’s value by 3.
+var remainder = myVariable % 3;
+
+// l. Output the remainder.
+document.write("The remainder is: " + remainder);
+
+    
+
+
+
+
+
+
+
+document.write("<h1>Rules For Naming JS Variables</h1></br><p>variables name can only contain, numbers,$ and _ for example $my , _1stvaraible</p>");
+document.write("<p>Variables must begin with a letter , $ and _For example $name, _name or name</p>")
+document.write("<p>Variable names are case sensitive</p>")
+document.write("<p>Variable names should not be JS Keywords</p>")
